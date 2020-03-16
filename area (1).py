4295191665454972dd96373220aeb7efc2bf71db
Shady-21 <60143577+Shady-21@users.noreply.github.com>
2020-03-16 16:33:33 +0200

    Add files via upload

---
+++ area (1).py
@@ -0,0 +1,15 @@
+#Name: Troy Vlahakis
+#date 16/03/2020
+import time
+
+print("************************Calculate the area of a square*************************")
+side=eval(input("Enter the side of the square \n :")) # Asking vaulue from user
+
+area=side*side  # Formula for area of a square 
+
+print("Calculating area.........")
+time.sleep(3)                       # 3 Second delay
+print("Area of square is  : ",area) #Prints to screen the area found 
+
+ # EXITING
+Quit = eval(input(" Please press any key to exit \n"))
