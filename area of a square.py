463182203938fa697fb7f8f7e5dbc52d90c36800
Shady-21 <60143577+Shady-21@users.noreply.github.com>
2020-03-16 16:01:17 +0200

    Add files via upload

---
+++ area of a square.py
@@ -0,0 +1,12 @@
+import time
+
+print("************************Calculate the area of a square*************************")
+side=int(input("Enter the side:"))
+
+area=side*side
+
+print("Calculating area.........")
+time.sleep(3)                       # 3 Second delay
+print("Area of square is  : ",area) #
+print("press any button to exit the program")
+exit();
